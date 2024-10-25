bcff28157de5e4eed33d8d33cf9ef0ffacd1caae
Chinh <64420055+maitanchinh@users.noreply.github.com>
2024-10-25 16:24:46 +0700

    Update Header.tsx

--- vietast-ecommerce/src/app/components/Header.tsx
+++ vietast-ecommerce/src/app/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from '../styles/Header.module.css';
+import Image from 'next/image';
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <img src="/assets/CTN-AVT.png" alt="Cá Tắm Nắng" className={styles.headerLogo} />
+      <Image src="/assets/CTN-AVT.png" alt="Cá Tắm Nắng" className={styles.headerLogo} />
       {/* <h1 className={styles.headerTitle}>Cá Tắm Nắng</h1> */}
     </header>
   );
